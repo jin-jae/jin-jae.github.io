9d83b8c02808d4736ac00fa45e5bd0a5d036727f
jin-jae <jinjae.dev@gmail.com>
2025-03-31 00:59:21 +0900

    fix: image and css render

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
       }
-    ]
-  }
+    ],
+    unoptimized: true,
+  },
+  output: 'export',
+  basePath: "",
+  assetPrefix: "/",
 };
 
 export default nextConfig;
